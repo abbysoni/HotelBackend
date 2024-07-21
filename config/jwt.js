c8715d6b190be201b779b20878e3d6deb8084dfb
unknown <a9094855@gmail.com>
2024-07-21 14:32:40 +0100

    jwt key error

--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
+const JWT_SECRET_KEY = process.env.JWT_SECRET || 12345
+
 //extract the jwt token from the request header
 const jwtAuthMiddleware = (req,res,next)=>{
     //first check if the request header have authorization
@@ -11,8 +13,8 @@ const token = req.headers.authorization.split(' ')[1];
 if(!token) return res.status(401).json({error: "Unauthorized"});
 try{
     //Verify the jwt token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("recieved jwt secret from env and token formed",process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
+    console.log("recieved jwt secret from env and token formed",JWT_SECRET_KEY)
 
     //Attach user information to the request object
     req.user = decoded;
@@ -24,9 +26,9 @@ try{
 
 //function to generate the jwt token
 const generateToken = (userData)=>{
-    console.log("data recieved at generate token:",userData,process.env.JWT_SECRET,{expiresIn:30000})
+    console.log("data recieved at generate token:",userData,JWT_SECRET_KEY,{expiresIn:30000})
     //Generate a new token using userData
-   return jwt.sign(userData, process.env.JWT_SECRET);
+   return jwt.sign(userData, JWT_SECRET_KEY);
 }
 
 module.exports = {jwtAuthMiddleware, generateToken};
